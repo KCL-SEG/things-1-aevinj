15a0a38dff126b0afdaf36f0aa20e2aca072e3b5
Aevin <ajjaevinjais@gmail.com>
2022-12-09 02:51:11 +0000

    rename model

--- things/models.py
+++ things/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class Things(models.Model):
+class Thing(models.Model):
     name = models.CharField(blank=False, max_length=12, unique=True)
     description = models.CharField(blank=True, max_length=30)
     quantity = models.IntegerField(blank=True)
